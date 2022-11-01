d7a9d94fe139bb0f8ec02703d04033881a02824f
wjh2012 <wjh2012@naver.com>
2022-11-01 22:05:10 +0900

    feat: 게시글 Controller 글작성

--- src/main/java/com/ggomg/myshop/post/controller/PostControllerImpl.java
+++ src/main/java/com/ggomg/myshop/post/controller/PostControllerImpl.java
@@ -1,7 +1,11 @@
 package com.ggomg.myshop.post.controller;
 
+import com.ggomg.myshop.post.controller.DTO.PostCreateRequestToController;
+import com.ggomg.myshop.post.service.DTO.PostCreateRequestToService;
 import com.ggomg.myshop.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +14,16 @@ public class PostControllerImpl {
 
     private final PostService postService;
 
+    @PostMapping("/createpost")
+    public Long savePost(@RequestBody PostCreateRequestToController request) {
+        return postService.save(
+                new PostCreateRequestToService(
+                        request.getBoard(),
+                        request.getCategory(),
+                        request.getMember(),
+                        request.getTitle(),
+                        request.getContent()));
+    }
 
 
 }
